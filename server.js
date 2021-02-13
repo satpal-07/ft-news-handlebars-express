c5ce6f2cc3213b76c670f4eab986d9b1fa20e4c2
Satpal <satpalsingh614@gmail.com>
2021-02-13 15:42:47 +0000

    trigger heroku deployment after port change

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 'use strict';
 const appConfig = require('config').app;
 const app = require('./app');
-
 // create server
-app.listen(appConfig.port, function () {
+app.listen(process.env.PORT || appConfig.port, function () {
   console.log(
     `The ${appConfig.serviceName} service is now running on port ${appConfig.port}`
   );
