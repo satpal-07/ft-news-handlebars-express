57c3318b09118634e14b1f787fca76dabf55a86f
Satpal <satpalsingh614@gmail.com>
2021-02-13 17:05:56 +0000

    added no result found page and error handling

--- controller/search.js
+++ controller/search.js
@@ -29,8 +29,9 @@ const endpoint = async (req, res, next) => {
     console.debug(`API response...`);
     const filteredData = filter(apiResponse.results[0].results, 'article');
     res.render('seach-page', {
-      data: filteredData || [],
+      data: filteredData,
       textInput: searchText,
+      isResultEmpty: isResultEmpty(filteredData),
     });
   } catch (error) {
     console.error(`Error in search controller: ${error.message}`);
@@ -41,8 +42,19 @@ const endpoint = async (req, res, next) => {
 };
 
 const filter = (data, field) => {
-  const filteredData = data.filter((elem) => elem.aspectSet === field);
-  return filteredData;
+  if (!data || !Array.isArray(data)) return [];
+  try {
+    const filteredData = data.filter((elem) => elem.aspectSet === field);
+    return filteredData;
+  } catch (error) {
+    console.error(`Error while filtering the search result: ${error.message}`);
+    return [];
+  }
+};
+
+const isResultEmpty = (result) => {
+  if (!result || !Array.isArray(result) || result.length === 0) return true;
+  return false;
 };
 
 module.exports = { endpoint, endpointName };
